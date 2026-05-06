2682382715310bd494c5e6dcd74454e471b75391
metallhobler <mwieber85@googlemail.com>
2026-05-06 23:17:27 +0200

    Update package.sidebar.php

--- src/Config/Menu/package.sidebar.php
+++ src/Config/Menu/package.sidebar.php
@@ -3,82 +3,26 @@
  *  Menu Configuration
  */
 return [
-    'corpminingtax' => [
+    'out-taxledger' => [
         'name' => 'menu-entry-name',
         'label' => 'Testmenu OUT',
         'plural' => true,
         'icon' => 'fas fa-certificate',
-        'route_segment' => 'corpminingtax',
+        'route_segment' => 'out-taxledger',
         'entries' => [
             [
-                'name' => 'corpminingtax-home-sub-menu',
-                'label' => 'corpminingtax::menu.sub-home-level',
+                'name' => 'out-taxledger-home-sub-menu',
+                'label' => 'out-taxledger::menu.sub-home-level',
                 'icon' => 'fas fa-home',
-                'route' => 'corpminingtax.home',
-                'permission' => 'corpminingtax.view'
+                'route' => 'out-taxledger.home',
+                'permission' => 'out-taxledger.view'
             ],
             [
-                'name' => 'corpminingtax-home-mining-log',
-                'label' => 'corpminingtax::menu.sub-mining-log',
+                'name' => 'out-taxledger-home-mining-log',
+                'label' => 'out-taxledger::menu.sub-mining-log',
                 'icon' => 'fas fa-table',
-                'route' => 'corpminingtax.logbook',
-                'permission' => 'corpminingtax.view'
-            ],
-            [
-                'name' => 'corpminingtax-sub-mining-tax',
-                'label' => 'corpminingtax::menu.sub-mining-tax',
-                'icon' => 'fas fa-money-bill-wave',
-                'route' => 'corpminingtax.tax',
-                'permission' => 'corpminingtax.manager'
-            ],
-            [
-                'name' => 'corpminingtax-sub-refining',
-                'label' => 'corpminingtax::menu.sub-refining',
-                'icon' => 'fas fa-hammer',
-                'route' => 'corpminingtax.refining',
-                'permission' => 'corpminingtax.view'
-            ],
-            [
-                'name' => 'corpminingtax-sub-corp-moon-mining',
-                'label' => 'corpminingtax::menu.sub-corp-moon-mining',
-                'icon' => 'fas fa-moon',
-                'route' => 'corpminingtax.corpmoonmining',
-                'permission' => 'corpminingtax.manager'
-            ],
-            [
-                'name' => 'corpminingtax-sub-tax-contracts',
-                'label' => 'corpminingtax::menu.sub-tax-contracts',
-                'icon' => 'fas fa-th-list',
-                'route' => 'corpminingtax.contracts',
-                'permission' => 'corpminingtax.settings'
-            ],
-            [
-                'name' => 'corpminingtax-sub-mining-events',
-                'label' => 'corpminingtax::menu.sub-mining-events',
-                'icon' => 'fas fa-calendar-alt',
-                'route' => 'corpminingtax.events',
-                'permission' => 'corpminingtax.settings'
-            ],
-            [
-                'name' => 'corpminingtax-sub-statistics',
-                'label' => 'corpminingtax::menu.sub-statistics',
-                'icon' => 'fas fa-chart-area',
-                'route' => 'corpminingtax.statistics',
-                'permission' => 'corpminingtax.settings'
-            ],
-            [
-                'name' => 'corpminingtax-sub-settings-menu',
-                'label' => 'corpminingtax::menu.sub-settings',
-                'icon' => 'fas fa-cogs',
-                'route' => 'corpminingtax.settings',
-                'permission' => 'corpminingtax.settings'
-            ],
-            [
-                'name' => 'corpminingtax-sub-thieves',
-                'label' => 'corpminingtax::menu.sub-thieves',
-                'icon' => 'fas fa-user',
-                'route' => 'corpminingtax.thieves',
-                'permission' => 'corpminingtax.settings'
+                'route' => 'out-taxledger.logbook',
+                'permission' => 'out-taxledger.view'
             ],
         ],
     ],
