009d52748b174bb2bcdc9188b781606da319ac72
metallhobler <mwieber85@googlemail.com>
2026-05-08 14:46:25 +0200

    Perm

--- src/Config/Menu/package.sidebar.php
+++ src/Config/Menu/package.sidebar.php
@@ -3,27 +3,41 @@
  *  Menu Configuration
  */
 return [
-    'out-taxledger' => [
-        'name' => 'home',
-        'label' => 'Test 1',
-        'icon' => 'fas fa-home',
-        'route_segment' => 'home',
-        'route' => 'seatcore::home',
-    ],
-    'out-taxledger2' => [
-        'name' => 'alliance',
-        'label' => 'Test 2',
+    'outsmarted' => [
+        'name' => 'outsmarted',
+        'label' => 'OUTsmarted',
         'plural' => true,
         'icon' => 'fas fa-city',
-        'route_segment' => 'test3',
-        'permission' => 'out-taxledger.personalminingtaxview',
+        'route_segment' => 'outsmarted',
+        'permission' => 'out-taxledger.generalmenu',
         'entries' => [
             [
-                'name' => 'all test3',
-                'label' => 'Test 3',
+                'name' => 'all outsmarted',
+                'label' => 'Mining Tax',
+                'icon' => 'fab fa-fort-awesome',
+                'route' => 'seatcore::alliance.list',
+                'permission' => 'out-taxledger.miningtaxview',
+            ],
+            [
+                'name' => 'all outsmarted',
+                'label' => 'Corp Ratting',
                 'icon' => 'fab fa-fort-awesome',
                 'route' => 'seatcore::alliance.list',
                 'permission' => 'out-taxledger.corprattingtaxview',
+            ],
+            [
+                'name' => 'all outsmarted',
+                'label' => 'Alliance Ratting',
+                'icon' => 'fab fa-fort-awesome',
+                'route' => 'seatcore::alliance.list',
+                'permission' => 'out-taxledger.alliancerattingtaxview',
+            ],
+            [
+                'name' => 'all outsmarted',
+                'label' => 'Settings',
+                'icon' => 'fab fa-fort-awesome',
+                'route' => 'seatcore::alliance.list',
+                'permission' => 'out-taxledger.settings',
             ],
         ],
     ],
--- src/Config/Permissions/out-taxledger.php
+++ src/Config/Permissions/out-taxledger.php
@@ -2,16 +2,22 @@
 
 
 return [
-    'personalminingtaxview' => [
+    'generalmenu' => [
+        'label' => 'Member',
+        'description' => 'Members have access to OUTSMARTED Menu',
+    ],
+    'miningtaxview' => [
         'label' => 'Member',
         'description' => 'Members have access to Overview and Reprocessing Tool',
     ],
-
     'corprattingtaxview' => [
-        'label' => 'Manager',
+        'label' => 'Corp Manager',
         'description' => 'View all alliance corporations rattings amounts and taxes',
     ],
-
+    'alliancerattingtaxview' => [
+        'label' => 'Alliance Manager',
+        'description' => 'View all alliance corporations rattings amounts and taxes',
+    ],
     'settings' => [
         'label' => 'Admin',
         'description' => 'Allow Admins to all settings and pages'
