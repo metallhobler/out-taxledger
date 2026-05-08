e637231acc695f74dcd7b7f8a6bd4775d0a18fcd
metallhobler <mwieber85@googlemail.com>
2026-05-08 15:10:11 +0200

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,12 +3,25 @@
 Route::group([
     'namespace'  => 'metallhobler\Seat\OUTTaxLedger\Http\Controllers',
     'middleware' => ['web', 'auth', 'locale'],
-], function (): void {
+], function () {
+    Route::prefix('/out-taxledger')
+        ->group(function () {
 
-    // Your route definitions go here.
-    Route::get('/outsmarted/taxledger', [
-        'as'   => 'out-taxledger.index',
-        'uses' => 'AllianceStructureController@index'
-    ]);
+            Route::get('/')
+                ->name('out-taxledger.home')
+                ->uses('CorpMiningOverviewController@getHome');
 
+            Route::get('/miningtax')
+                ->name('out-taxledger.miningtax')
+                ->uses('CorpMiningLog@index');
+            Route::get('/corprat')
+                ->name('out-taxledger.corprat')
+                ->uses('CorpMiningLog@index');
+            Route::get('/alliancerat')
+                ->name('out-taxledger.alliancerat')
+                ->uses('CorpMiningLog@index');
+            Route::get('/settings')
+                ->name('out-taxledger.settings')
+                ->uses('CorpMiningLog@index');
+        });
 });
