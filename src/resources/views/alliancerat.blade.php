01f53956a069fdb579a729654d84272faccd1744
metallhobler <mwieber85@googlemail.com>
2026-05-14 20:55:24 +0200

    Update alliancerat.blade.php

--- src/resources/views/alliancerat.blade.php
+++ src/resources/views/alliancerat.blade.php
@@ -65,12 +65,29 @@
                         <th>Hidden</th>
                     </tr>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        @foreach ($entries as $entry)
+                            <tr>
+                            <td data-order="{{ $entry->second_party->name }}">
+                                @switch($entry->second_party->category)
+                                @case('character')
+                                    @include('web::partials.character', ['character' => $entry->second_party])
+                                @break
+                                @case('corporation')
+                                    @include('web::partials.corporation', ['corporation' => $entry->second_party])
+                                @break
+                                @case('alliance')
+                                    @include('web::partials.alliance', ['alliance' => $entry->second_party])
+                                @break
+                                @endswitch
+                            </td>
+                            <td data-order="{{ $entry->total }}">{{ number_format($entry->total) }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </table>
                 <p>
-                    @php
-                     print_r($entries)   
-                    @endphp
+                    
                     </p>
             </div>
         </div>
