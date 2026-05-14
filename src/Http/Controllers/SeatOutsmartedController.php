35141f5b3bdb87ded48d2a1f20b307cf470f3246
metallhobler <mwieber85@googlemail.com>
2026-05-14 16:44:47 +0200

    Create SeatOutsmartedController.php

---
+++ src/Http/Controllers/SeatOutsmartedController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace metallhobler\Seat\OUTTaxLedger\Http\Controllers;
+
+use Seat\Web\Http\Controllers\Controller;
+use Seat\Eveapi\Models\Character\CharacterInfo;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Js;
+use Datatables;
+
+class SeatOutsmartedController extends Controller
+{
+    public function index()
+    {
+        $character = auth()->user()->main_character['character_id'];
+        //$characters = CharacterHelper::getLinkedCharacters($character);
+        $miningdata = DB::table('corp_mining_tax as cm')
+            ->select('*') 
+            ->join('character_infos as ci', 'ci.character_id', 'cm.character_id')
+            ->where('main_character_id', '=', $character)
+            ->orderBy('year', 'asc')
+            ->orderBy('month', 'asc')
+            ->get();
+        return view('corpminingtax::corpmininglog', ['miningdata' => $miningdata]);
+    }
+}
