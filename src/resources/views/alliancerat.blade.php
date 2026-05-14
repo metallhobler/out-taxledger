4a4d89317a73d990fa2fb5bc211ec922b3949ce5
metallhobler <mwieber85@googlemail.com>
2026-05-14 22:24:29 +0200

    Update alliancerat.blade.php

--- src/resources/views/alliancerat.blade.php
+++ src/resources/views/alliancerat.blade.php
@@ -20,17 +20,7 @@
                     @foreach ($entries as $entry)
                         <tr>
                         <td data-order="{{ $entry->second_party->name }}">
-                            @switch($entry->second_party->category)
-                            @case('character')
-                                @include('web::partials.character', ['character' => $entry->second_party])
-                            @break
-                            @case('corporation')
-                                @include('web::partials.corporation', ['corporation' => $entry->second_party])
-                            @break
-                            @case('alliance')
-                                @include('web::partials.alliance', ['alliance' => $entry->second_party])
-                            @break
-                            @endswitch
+                            @include('web::partials.corporation', ['corporation' => $entry->corporation_id])
                         </td>
                         
                         <td data-order="{{ $entry->total }}">{{ number_format($entry->total) }}
