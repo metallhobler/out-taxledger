1e50bbb136f456d2a2fde5e6afcc5b66065d92a7
metallhobler <mwieber85@googlemail.com>
2026-05-14 22:47:03 +0200

    Update alliancerat.blade.php

--- src/resources/views/alliancerat.blade.php
+++ src/resources/views/alliancerat.blade.php
@@ -7,12 +7,38 @@
         <div class="card-header">
             <h4>Alliance Taxes</h4>
         </div>
+        <div class="card-body">
+
+        @foreach ($periods->chunk(12) as $chunk)
+            <ul class="nav nav-pills justify-content-between">
+
+            @foreach ($chunk as $period)
+                <li class="nav-item">
+                @if(date('Y', strtotime($period->year . '-01-01')) == (request()->route()->parameter('year') ?: carbon()->isoFormat('YYYY')) && date('m', strtotime($period->year . '-' . $period->month . '-01')) == (request()->route()->parameter('month') ?: carbon()->isoFormat('MM')))
+                    <a href="{{ route('seat-outsmarted::alliancerat', [
+                        $corporation,
+                        date('Y', strtotime($period->year. '-01-01')),
+                        date('m', strtotime($period->year . '-' . $period->month . '-01'))
+                    ]) }}" class="nav-link active">{{ date("M Y", strtotime(sprintf('%d-%d-01', $period->year, $period->month))) }}</a>
+                @else
+                    <a href="{{ route('seat-outsmarted::alliancerat', [
+                        $corporation,
+                        date('Y', strtotime($period->year. '-01-01')),
+                        date('m', strtotime($period->year . '-' . $period->month . '-01'))
+                    ]) }}" class="nav-link">{{ date("M Y", strtotime(sprintf('%d-%d-01', $period->year, $period->month))) }}</a>
+                @endif
+                </li>
+            @endforeach
+
+            </ul>
+        @endforeach
+        </div>
 
         <div class="card-body">
             <table class="table" id="events">
                 <thead>
                 <tr>
-                    <th>{{ trans_choice('web::seat.alliance', 1) }}</th>
+                    <th>{{ trans_choice('web::seat.corporation', 1) }}</th>
                     <th>{{ trans_choice('web::seat.total', 1) }}</th>
                 </tr>
                 </thead>
