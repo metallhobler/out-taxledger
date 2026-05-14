9057b0d3f420dce49709efb97268beb40d55a7f8
metallhobler <mwieber85@googlemail.com>
2026-05-14 19:35:21 +0200

    Update SeatOutsmartedController.php

--- src/Http/Controllers/SeatOutsmartedController.php
+++ src/Http/Controllers/SeatOutsmartedController.php
@@ -104,7 +104,7 @@ class SeatOutsmartedController extends Controller
         $periods = $this->getCorporationLedgerPeriods($ref_types);
         $entries = $this->getCorporationLedgerByMonth($group_column, $ref_types, $year, $month);
 
-        return view('seat-outsmarted::alliancerat',
+        return view('alliancerat',
             compact('periods', 'entries', 'month', 'year'));
     }
 
