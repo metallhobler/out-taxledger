e2b7ddc537c53f1ea6c63c8cc1b582ebe1a99f22
metallhobler <mwieber85@googlemail.com>
2026-07-13 22:14:10 +0200

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,28 +5,26 @@ Route::group([
     'prefix' => 'outsmarted',
     'middleware' => ['web', 'auth', 'locale'],
 ], function () {
-    Route::prefix('/outsmarted')
-        ->group(function () {
 
-            Route::get('/')
+
+    /*Route::get('/')
                 ->name('seat-outsmarted::home')
                 ->uses('SeatOutsmartedController@index');
 
-            /* Route::get('/miningtax')
+            Route::get('/miningtax')
                 ->name('seat-outsmarted::miningtax')
                 ->uses('SeatOutsmartedController@miningtax');
             */
-            Route::get('/corprat')
-                ->name('seat-outsmarted::corprat')
-                ->uses('SeatOutsmartedController@corprat');
+    Route::get('/corprat')
+        ->name('seat-outsmarted::corprat')
+        ->uses('SeatOutsmartedController@corprat');
 
-            Route::get('/alliancerat/{year?}/{month?}')
-                ->name('seat-outsmarted::alliancerat')
-                ->uses('SeatOutsmartedController@getBountyPrizesByMonth');
+    Route::get('/alliancerat/{year?}/{month?}')
+        ->name('seat-outsmarted::alliancerat')
+        ->uses('SeatOutsmartedController@getBountyPrizesByMonth');
 
-            /*Route::get('/settings')
+    /*Route::get('/settings')
                 ->name('seat-outsmarted::settings')
                 ->uses('SeatOutsmartedController@settings');
             */
-        });
 });
